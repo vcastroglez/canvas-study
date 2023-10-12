60fae6fe94afcb816eefda7deb2305a88c30c924
vgonzales <vcastroglez@gmail.com>
2023-10-12 21:34:12 +0300

    Moving tracker to player and some more refactor

--- src/socket.php
+++ src/socket.php
@@ -2,6 +2,6 @@
 require_once 'bootstrap.php';
 require_once SRC_DIR.'/router-socket.php';
 
-$ws_server = new \app\CentralServer('piupiu.alpec.cu', 8080);
+$ws_server = new \app\CentralServer('piupiu.kama.com', 8080);
 \src\redis\RedisConnection::i()->cleanOldUsers(true);
 $ws_server->run();
