592210fa5c435f82c6cd616432731569c6ee89e8
vgonzales <vcastroglez@gmail.com>
2023-10-12 22:52:53 +0300

    env file, basic blade and caching views

--- src/socket.php
+++ src/socket.php
@@ -2,6 +2,6 @@
 require_once 'bootstrap.php';
 require_once SRC_DIR.'/router-socket.php';
 
-$ws_server = new \app\CentralServer('81.28.6.236', 8080);
+$ws_server = new \app\CentralServer(env('WS_DNS'), 8080);
 \src\redis\RedisConnection::i()->cleanOldUsers(true);
 $ws_server->run();
